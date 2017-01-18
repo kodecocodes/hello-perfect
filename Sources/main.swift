32bf82682d21693a49048dca06c4a1f84127ef26
Ray Wenderlich <ray.wenderlich@gmail.com>
2017-01-18 16:01:09 -0500

    Initial commit.

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,86 +1,62 @@
-//
-//  main.swift
-//  PerfectTemplate
-//
-//  Created by Kyle Jessup on 2015-11-05.
-//	Copyright (C) 2015 PerfectlySoft, Inc.
-//
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the Perfect.org open source project
-//
-// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
-// Licensed under Apache License v2.0
-//
-// See http://perfect.org/licensing.html for license information
-//
-//===----------------------------------------------------------------------===//
-//
-
 import PerfectLib
 import PerfectHTTP
 import PerfectHTTPServer
 
-// An example request handler.
-// This 'handler' function can be referenced directly in the configuration below.
-func handler(data: [String:Any]) throws -> RequestHandler {
-	return {
-		request, response in
-		// Respond with a simple message.
-		response.setHeader(.contentType, value: "text/html")
-		response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
-		// Ensure that response.completed() is called when your processing is done.
-		response.completed()
-	}
+let server = HTTPServer()
+var routes = Routes()
+
+func returnJSONMessage(message: String, response: HTTPResponse) {
+  response.setHeader(.contentType, value: "application/json")
+  do {
+    try response.setBody(json: ["message": message])
+  } catch {
+    response.status = .internalServerError
+    response.setBody(string: "Error handling request: \(error)")
+  }
+  response.completed()
 }
 
-// Configuration data for two example servers.
-// This example configuration shows how to launch one or more servers 
-// using a configuration dictionary.
+routes.add(method: .get, uri: "/", handler: {
+  request, response in
+  returnJSONMessage(message: "Hello, Perfect!", response: response)
+})
 
-let port1 = 8080, port2 = 8181
+routes.add(method: .get, uri: "/hello", handler: {
+  request, response in
+  returnJSONMessage(message: "Hello, again!", response: response)
+})
 
-let confData = [
-	"servers": [
-		// Configuration data for one server which:
-		//	* Serves the hello world message at <host>:<port>/
-		//	* Serves static files out of the "./webroot"
-		//		directory (which must be located in the current working directory).
-		//	* Performs content compression on outgoing data when appropriate.
-		[
-			"name":"localhost",
-			"port":port1,
-			"routes":[
-				["method":"get", "uri":"/", "handler":handler],
-				["method":"get", "uri":"/**", "handler":PerfectHTTPServer.HTTPHandler.staticFiles,
-				 "documentRoot":"./webroot",
-				 "allowResponseFilters":true]
-			],
-			"filters":[
-				[
-				"type":"response",
-				"priority":"high",
-				"name":PerfectHTTPServer.HTTPFilter.contentCompression,
-				]
-			]
-		],
-		// Configuration data for another server which:
-		//	* Redirects all traffic back to the first server.
-		[
-			"name":"localhost",
-			"port":port2,
-			"routes":[
-				["method":"get", "uri":"/**", "handler":PerfectHTTPServer.HTTPHandler.redirect,
-				 "base":"http://localhost:\(port1)"]
-			]
-		]
-	]
-]
+routes.add(method: .get, uri: "/hello/there", handler: {
+  request, response in
+  returnJSONMessage(message: "I am tired of saying hello!", response: response)
+})
+
+routes.add(method: .get, uri: "/beers/{num_beers}", handler: {
+  request, response in
+  guard let numBeersString = request.urlVariables["num_beers"],
+    let numBeersInt = Int(numBeersString) else {
+    response.status = .badRequest
+    response.completed()
+    return
+  }
+  returnJSONMessage(message: "Take one down, pass it around, \(numBeersInt - 1) bottles of beer on the wall...", response: response)
+})
+
+routes.add(method: .post, uri: "post", handler: {
+  request, response in
+  guard let name = request.param(name: "name") else {
+    response.status = .badRequest
+    response.completed()
+    return
+  }
+  returnJSONMessage(message: "Hello, \(name)!", response: response)
+})
+
+server.addRoutes(routes)
+server.serverPort = 8080
 
 do {
-	// Launch the servers based on the configuration data.
-	try HTTPServer.launch(configurationData: confData)
-} catch {
-	fatalError("\(error)") // fatal error launching one of the servers
+  try server.start()
+} catch PerfectError.networkError(let err, let msg) {
+  print("Network error thrown: \(err) \(msg)")
 }
-
